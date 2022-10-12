4494cce5ba64fcacf6d42bb7a3d3d8315902d9b4
sazalislam04 <mdsazalislam04@gmail.com>
2022-10-12 00:39:51 -0600

    added blog page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
+import AOS from "aos";
+import "aos/dist/aos.css";
+import { useEffect } from "react";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routes/router";
-
 function App() {
+  useEffect(() => {
+    AOS.init();
+    AOS.refresh();
+  }, []);
   return (
     <div>
       <RouterProvider router={router} />
